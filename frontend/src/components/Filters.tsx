aee60e933f2a33f5b7de0d675f28b0d531c3fd14
kyler505 <kylercao18@gmail.com>
2026-04-02 15:20:10 -0500

    fix navigation shell layout bugs

--- frontend/src/components/Filters.tsx
+++ frontend/src/components/Filters.tsx
@@ -58,7 +58,6 @@ export default function Filters({
                 width: tabRect.width,
             });
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [status]);
 
     return (
