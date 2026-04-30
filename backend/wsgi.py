58125e0445415bf06f6fbb2339d5e8e1d12ac455
kyler505 <kylercao18@gmail.com>
2026-04-29 22:04:56 -0500

    fix: make wsgi.py project_path relative for dev/prod compatibility

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 # Add project to path
-project_path = '/home/techhub/techhub-dns/backend'
+project_path = os.path.dirname(os.path.abspath(__file__))
 if project_path not in sys.path:
     sys.path.insert(0, project_path)
 
